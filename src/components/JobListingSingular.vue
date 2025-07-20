8f154780e5285c8b7d4e0a7ab05c60b77e97e56b
seren <seren.kaya@turboard.com>
2025-07-20 23:55:30 +0300

    ADDIN: added show more button to jobs description

--- src/components/JobListingSingular.vue
+++ src/components/JobListingSingular.vue
@@ -7,7 +7,12 @@
             </div>
 
             <div class="mb-5">
-                {{ job.description }}
+                <div>
+                    {{ truncatedDescription }}
+                </div>
+                <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+                    {{ showFullDescription ? 'Show Less' : 'Show More' }}
+                </button>
             </div>
 
             <h3 class="text-green-500 mb-2">{{ job.salary }}</h3>
@@ -29,7 +34,7 @@
 </template>
 
 <script>
-
+import { computed, ref } from 'vue';
 export default {
     name: 'JobListingSingular',
     props: {
@@ -39,7 +44,22 @@ export default {
         }
     },
     setup(props) {
-        return {};
+        const showFullDescription = ref(false);
+        const toggleFullDescription = () => {
+            showFullDescription.value = !showFullDescription.value;
+        };
+        const truncatedDescription = computed(() => {
+            let description = props.job.description;
+            if (!showFullDescription.value) {
+                description = description.substring(0, 90) + '...';
+            }
+            return description;
+        })
+        return {
+            truncatedDescription,
+            showFullDescription,
+            toggleFullDescription,
+        };
     }
 };
 
