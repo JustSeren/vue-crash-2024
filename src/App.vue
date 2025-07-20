26813d4de5ddbf39e6910af0f13d73174dadd56c
seren <seren.kaya@turboard.com>
2025-07-20 14:39:33 +0300

    for loops and links using binds

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
   <p v-if="status === 'active'"> User is <strong>active</strong></p>
   <p v-else-if="status === 'pending'"> User is <strong>pending</strong></p>
   <p v-else> User is <strong>inactive</strong></p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      <span :class="{ completed: task.completed }">{{ task.title }}</span>
+    </li>
+  </ul>
+  <a v-bind:href="link" target="_blank">Vue.js Documentation</a>
+  <a :href="link" target="_blank">Vue.js Documentation without v-bind</a>
 </template>
 
 <script>
@@ -10,7 +19,13 @@
     data () {
       return {
         name: 'I\'m Seren',
-        status: 'pending'
+        status: 'pending',
+        tasks: [
+          { id: 1, title: 'Task 1', completed: true },
+          { id: 2, title: 'Task 2', completed: false },
+          { id: 3, title: 'Task 3', completed: true },
+        ],
+        link: 'https://vuejs.org/',
       }
     },
   }
