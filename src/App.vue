7e21664011cc8c9c879fd1ed5d689a7e458c8ea8
seren <seren.kaya@turboard.com>
2025-07-20 22:46:39 +0300

    ADDİNG: added props for title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <Navbar />
+  <Hero title="Test Title" subtitle="Test Subtitle"/>
 </template>
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
