8c2c44fd4ca78f48fa25ef564e63fc2ce50ee047
seren <seren.kaya@turboard.com>
2025-07-20 14:29:03 +0300

    v-if and else condition with boolean feature

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>{{name}}</h1>
+  <p v-if="status"> User is <strong>active</strong></p>
+  <p v-else> User is <strong>inactive</strong></p>
 </template>
 
 <script>
   export default{
     data () {
       return {
-        name: 'I\'m Seren'
+        name: 'I\'m Seren',
+        status: false
       }
     },
   }
