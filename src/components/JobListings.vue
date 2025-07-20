2409581e403fa67c7efac1fe75661c43455866a3
seren <seren.kaya@turboard.com>
2025-07-20 23:45:56 +0300

    add comment

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -9,6 +9,7 @@
             </div>
         </div>
     </section>
+    <!-- buttın not goes jobs ıdk why stilll show limitid job  -->
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
         <a href="jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
             All Jobs</a>
