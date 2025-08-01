83345422d0f76ee38c57ee452e51d06d3a52b46e
seren <seren.kaya@turboard.com>
2025-08-02 01:59:32 +0300

    ADDDED: router to app pages

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <Hero title="Test Title" subtitle="Test Subtitle" />
+    <HomeCards />
+    <JobListings :limit="3" :showButton="true" />
+</template>
+<script>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
